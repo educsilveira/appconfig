b8cdc0ea9c2b6665f5c6d67caf0e9990cd154010
eduscosta_@hotmail.com <eduscosta_@hotmail.com>
2023-02-27 22:40:55 -0300

    montando tela de login

--- paginas/urls.py
+++ paginas/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from paginas.views import IndexView
+from paginas.views import IndexView,PaginaInicial,SobreView, loginView
 
 urlpatterns = [
-    path( 'inicio/', IndexView.as_view(), name='Inicio'),
+    path( '', IndexView.as_view(), name='Home'),
+    path( '', PaginaInicial.as_view(), name='conteudo'),
+    path( '', SobreView.as_view(), name='sobre'),
+    path( '', loginView.as_view(), name='login'),
 ]
 
--- paginas/views.py
+++ paginas/views.py
@@ -1,5 +1,15 @@
+""" Importar o TemplateView para criar páginas simples """
 from django.views.generic import TemplateView
 
-# Create your views here.
+""" Create your views here. """
 class IndexView(TemplateView):
     template_name = "index.html"
+
+class PaginaInicial(TemplateView):
+    template_name = "modelo.html"
+
+class SobreView(TemplateView):
+    template_name = "sobre.html"
+
+class loginView(TemplateView):
+    template_name = "login.html"    
